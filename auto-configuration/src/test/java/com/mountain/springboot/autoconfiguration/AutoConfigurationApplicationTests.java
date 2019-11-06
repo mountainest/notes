e00410d71e5a40b8b915d066b52469f6538ef82c
mountainest <mountainest@sina.com>
2019-11-06 22:39:54 +0800

    初始化demo工程

---
+++ auto-configuration/src/test/java/com/mountain/springboot/autoconfiguration/AutoConfigurationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mountain.springboot.autoconfiguration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoConfigurationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
