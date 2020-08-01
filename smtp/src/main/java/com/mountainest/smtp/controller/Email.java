1272abf13771ee8831eed56fe815c518729554cd
mountainest <mountainest@sina.com>
2020-08-01 19:25:32 +0800

    邮件发送成功

--- smtp/src/main/java/com/mountainest/smtp/controller/Email.java
+++ smtp/src/main/java/com/mountainest/smtp/controller/Email.java
@@ -1,17 +1,21 @@
 package com.mountainest.smtp.controller;
 
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.boot.autoconfigure.mail.MailProperties;
-//import org.springframework.mail.javamail.JavaMailSender;
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.mail.MailProperties;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Email {
-    // @Autowired
-    // private MailProperties mailProperties;
-    // @Autowired
-    // private JavaMailSender mailSender;
+    @Autowired
+    private MailProperties mailProperties;
+    @Autowired
+    private JavaMailSender mailSender;
 
     @PostMapping(value = "/send/email")
     public boolean SendEmail() {
@@ -19,7 +23,23 @@ public class Email {
     }
 
     private boolean send() {
-        //MimeMessage mimeMessage = mailSender.createMimeMessage();
+        MimeMessage mimeMessage = mailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(mimeMessage);
+
+        try {
+            helper.setFrom(mailProperties.getUsername());
+            helper.setTo("pfanr@163.com");
+            helper.setSubject("subject");
+            helper.setText("arg0", true);
+            
+            mailSender.send(mimeMessage);
+        } catch (MessagingException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+
+            return false;
+        }
+
         return true;
     }
 }
