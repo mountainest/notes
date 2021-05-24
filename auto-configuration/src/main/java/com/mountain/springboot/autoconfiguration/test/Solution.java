c6917b255a86c9532e544da5aeaba2867148192d
mountainest <mountainest@sina.com>
2021-05-24 19:10:54 +0800

    增加答题框架

---
+++ auto-configuration/src/main/java/com/mountain/springboot/autoconfiguration/test/Solution.java
@@ -0,0 +1,5 @@
+package com.mountain.springboot.autoconfiguration.test;
+
+public class Solution {
+
+}
---
+++ auto-configuration/src/test/java/com/mountain/springboot/autoconfiguration/test/SolutionTest.java
@@ -0,0 +1,11 @@
+package com.mountain.springboot.autoconfiguration.test;
+
+import com.mountain.springboot.autoconfiguration.AutoConfigurationApplicationTests;
+import org.junit.Test;
+
+public class SolutionTest extends AutoConfigurationApplicationTests {
+
+  @Test
+  public void test() {
+  }
+}
