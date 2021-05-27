d998e171e6a91bb334ef52f0c2d1e1760a0afc7e
mountainest <mountainest@sina.com>
2021-05-27 21:26:40 +0800

    新增demo工程

---
+++ demo/src/Solution.java
@@ -0,0 +1,5 @@
+public class Solution {
+    public void print() {
+        System.out.println("测试");
+    }
+}
---
+++ demo/src/SolutionTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class SolutionTest {
+
+  @Test
+  void test01() {
+
+    new Solution().print();
+    Assertions.assertTrue(3>1);
+  }
+}
