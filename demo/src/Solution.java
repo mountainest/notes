5b10fcd76bc37fd86b8495de3eefcaa1475678c1
mountainest <mountainest@sina.com>
2021-05-27 22:41:23 +0800

    3. 无重复字符的最长子串

--- demo/src/Solution.java
+++ demo/src/Solution.java
@@ -1,5 +1,49 @@
+import java.util.HashSet;
+
 public class Solution {
-    public void print() {
-        System.out.println("测试");
+    public static int lengthOfLongestSubstring(String s) {
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+
+        HashSet<Character> set = new HashSet<>();
+        int i = 0;
+        int j = 1;
+        int max = 1;
+        set.add(s.charAt(i));
+
+        while (j < s.length()) {
+            Character ch = s.charAt(j);
+            // 如果不重复，往集合添加元素，右指针右移
+            if (!set.contains(ch)) {
+                set.add(ch);
+                j++;
+                continue;
+            }
+
+            // 否则，计算最大值。
+            if (j - i > max) {
+                max = j - i;
+            }
+
+            // 左指针移动，并删除元素，直到碰见相等的。
+            while (i < j) {
+                if (s.charAt(i) == ch) {
+                    i++;
+                    break;
+                } else {
+                    set.remove(s.charAt(i));
+                    i++;
+                }
+            }
+
+            j++;
+        }
+
+        if (j - i > max) {
+            max = j - i;
+        }
+
+        return max;
     }
 }
