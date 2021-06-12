d585d86a5448dbbe2b5c20e6304b90fa069fe9e5
mountainest <mountainest@sina.com>
2021-06-12 11:38:21 +0800

    转成数组

--- demo/src/Puke.java
+++ demo/src/Puke.java
@@ -1,9 +1,8 @@
 import java.util.LinkedList;
-import java.util.List;
 
 // 求扑克牌的原始顺序
 public class Puke {
-  public List<Integer> get(int[] input) {
+  public Integer[] get(int[] input) {
     int size = input.length;
     LinkedList<Integer> q = new LinkedList<>();
     q.addFirst(input[size - 1]);
@@ -14,6 +13,6 @@ public class Puke {
       q.addFirst(input[size - i - 1]);
     }
 
-    return q;
+    return q.toArray(new Integer[size]);
   }
 }
