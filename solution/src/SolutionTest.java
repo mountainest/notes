80010f511c733bd15fcaa14ca885a39444d328ac
mountain <mountainest@sina.com>
2022-10-19 11:04:35 +0800

    野村

--- solution/src/SolutionTest.java
+++ solution/src/SolutionTest.java
@@ -6,7 +6,48 @@ class SolutionTest {
   @Test
   void test01() {
 
-    new Solution().print();
-    Assertions.assertTrue(3>1);
+    int[] a = {1, 2, 1};
+    int result = new Solution().solution(a);
+    Assertions.assertTrue(result == 2);
+  }
+
+  @Test
+  void test02() {
+
+    int[] a = {2, 1, 4, 4};
+    int result = new Solution().solution(a);
+    Assertions.assertTrue(result == 1);
+  }
+
+  @Test
+  void test03() {
+
+    int[] a = {6, 2, 3, 5, 6, 3};
+    int result = new Solution().solution(a);
+    Assertions.assertTrue(result == 4);
+  }
+
+  @Test
+  void test04() {
+
+    int[] a = {2,1,3};
+    int result = new Solution().solve(a, 2);
+    Assertions.assertTrue(result == 3);
+  }
+
+  @Test
+  void test05() {
+
+    int[] a = {0,4,3, -1};
+    int result = new Solution().solve(a, 2);
+    Assertions.assertTrue(result == 2);
+  }
+
+  @Test
+  void test06() {
+
+    int[] a = {2,1,4};
+    int result = new Solution().solve(a, 3);
+    Assertions.assertTrue(result == 0);
   }
 }
