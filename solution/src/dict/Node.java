9be4cf13d65e58493f960627ea624f2f03e8021a
mountain <mountainest@sina.com>
2022-11-27 23:08:51 +0800

    字典查找

---
+++ solution/src/dict/Node.java
@@ -0,0 +1,23 @@
+package dict;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Node {
+    /**
+     * 如果是尾节点，key为null
+     */
+    private Map<String, Node> nodes = new HashMap<>();
+
+    public boolean containes(String ch) {
+        return this.nodes.containsKey(ch);
+    }
+
+    public Node getNext(String ch) {
+        return this.nodes.get(ch);
+    }
+
+    public void addNext(String ch, Node node) {
+        this.nodes.put(ch, node);
+    }
+}
